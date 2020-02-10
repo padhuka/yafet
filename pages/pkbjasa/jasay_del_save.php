51877ece113986e9c92b1d2f4c02e20a58693a68
dapras <dapras.solo@gmail.com>
2020-02-11 05:31:46 +0700

    total

--- pages/pkbjasa/jasay_del_save.php
+++ pages/pkbjasa/jasay_del_save.php
@@ -8,9 +8,9 @@
         mysql_query($sqlhapusasuransi);
         //echo $sqlhapusasuransi;
 
-       $sqljasa= "SELECT (sum(harga_total_paket_jasa)+sum(harga_total_jasa)) AS totpaketjasa FROM t_pkb_jasa_detail WHERE fk_pkb_jasa = '$idpkbjasa'";
+        $sqljasa= "SELECT sum(harga_total_paket_jasa) AS totpaketjasa, sum(harga_total_jasa) AS totjasa FROM t_pkb_jasa_detail WHERE fk_pkb_jasa = '$idpkbjasa'";
                       $hjasa= mysql_fetch_array(mysql_query($sqljasa));
-                      $totjasa=$hjasa['totpaketjasa'];
+                      $totjasa=$hjasa['totpaketjasa'] + $hjasa['totjasa'];
                       //echo $sqljasa;
                       //exit();
                       $updatejasa = "UPDATE t_pkb_jasa set total_netto_harga_jasa='$totjasa' WHERE id_pkb_jasa='$idpkbjasa'";
