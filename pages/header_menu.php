b82778a7a41da38c9accaa79c47cf779d0e432b1
dapras <dapras.solo@gmail.com>
2020-02-12 19:49:53 +0700

    menu

--- pages/header_menu.php
+++ pages/header_menu.php
@@ -1,9 +1,8 @@
 
 <div class="wrapper">
-
   <header class="main-header">
- 
-    <!-- Header Navbar: style can be found in header.less -->
+            
+      <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar fixed-top" style="margin-left: 0px" >
       <!-- Sidebar toggle button-->
     <div class="container">
@@ -12,112 +11,91 @@
       <div class="navbar-custom-menu" style="padding: 0 0 0 0;margin: 0 auto;">
         <ul class="nav navbar-nav" style="list-style: none;padding: 0;margin: 0 auto;font-size: 1px;margin-top: 8px;">
          
-          <li style="display: block;margin:0;padding:0;float: left;"><a href="index.php" style="width: 100%;padding: 0.5em;"><button class="btn btn-default btne" style="padding: 5px 10px;background-color:white;">Home <i class="fa fa-home"></i></button></a></li>
+          <li style="display: block;margin:0;padding:0;float: left;"><a href="index.php" style="width: 100%;padding: 0.5em;"><button class="btn btn-default btne" style="padding: 5px 10px;background-color:white;">HOME <i class="fa fa-home"></i></button></a></li>
+          
           <?php if ($seskdlvl=='Umum' || $seskdlvl=='Admin'){?>
-          <li style="display: block;margin:0;padding:0;float: left;"><a href="?p=inventory" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Kartu Pelanggan <i class="fa fa-user-plus"></i></button></a></li>
+          <li style="display: block;margin:0;padding:0;float: left;"><a href="?p=inventory" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">KARTU PELANGGAN <i class="fa fa-user-plus"></i></button></a></li>
           <?php } ?>
-          <?php if ($seskdlvl=='Umum' || $seskdlvl=='Admin'){?>
-          <li style="display: block;margin:0;padding:0;float: left;"><a href="?p=estimasi" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Estimasi <i class="fa fa-file-text"></i></button></a></li>
-          <?php }?>
 
           <?php if ($seskdlvl=='Admin'){?>
            <li class="dropdown" style="display: block;margin:0;padding:0;float: left;">
-            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">PKB BR <i class="fa fa-file-text dropdown-toggle"></i></button> </a>
+            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">BODY REPAIR <i class="fa fa-file-text dropdown-toggle"></i></button> </a>
             <ul class="dropdown-menu">
-              <li class="header"><a href="?p=pkb">PKB BR</a></li> 
-              <li class="header"><a href="?p=pkbbukatutup">Buka/Tutup PKB BR</a></li>    
-                          </ul>
-          </li><?php } ?>
+              <li><a href="?p=estimasi">ESTIMASI</a></li>
+              <li><a href="?p=pkb">PKB BR</a></li> 
+              <li><a href="?p=pkbbukatutup">BUKA/TUTUP PKB</a></li>   
+              <li class="dropdown-submenu">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">FINANCE</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="?p=kwitansi">KWITANSI BR</a></li> 
+                                        <li><a href="?p=kwitansior">KWITANSI OR</a></li>   
+                                        <li><a href="?p=cash">CASH</a></li> 
+                                        <li><a href="?p=bank">BANK</a></li> 
+                                        <li><a href="?p=gatepass">GATE PASS</a></li> 
+                                    </ul>
+                                </li>
+            </ul>
+          </li>
+          <?php } ?>
 
           <?php if ($seskdlvl=='Admin'){?>
            <li class="dropdown" style="display: block;margin:0;padding:0;float: left;">
-            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">PKB GR <i class="fa fa-file-text dropdown-toggle"></i></button> </a>
-            <ul class="dropdown-menu">
-              <li class="header"><a href="?p=pkbjasa">PKB Jasa</a></li>
-              <li class="header"><a href="?p=pkbjasabukatutup">Buka/Tutup PKB Jasa</a></li>
-              <li class="header"><a href="?p=nonpkb">Non PKB</a></li>
-                          </ul>
-          </li><?php } ?>
+            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">GENERAL REPAIR <i class="fa fa-file-text dropdown-toggle"></i></button> </a>
+            <ul class="dropdown-menu">              
+              <li><a href="?p=pkbjasa">PKB GR</a></li>
+              <li><a href="?p=pkbjasabukatutup">BUKA/TUTUP PKB</a></li>
+              <li><a href="?p=nonpkb">NON PKB</a></li>
+              <li class="dropdown-submenu">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">FINANCE</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="?p=kwitansigr">KWITANSI GR</a></li> 
+                                        <li><a href="?p=cashgr">CASH</a></li> 
+                                        <li><a href="?p=bankgr">BANK</a></li> 
+                                        <li><a href="?p=gatepassgr">GATE PASS</a></li> 
+                                        <li><a href="?p=paycuci">PAYMENT NON PKB</a></li>  
+                                    </ul>
+                                </li>
+            </ul>
+          </li>
+          <?php } ?>
 
-          <?php if ($seskdlvl=='Umum' || $seskdlvl=='Admin'){?>
-          <li style="display: block;margin:0;padding:0;float: left;"><a href="?p=panel" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Panel <i class="fa fa-window-restore"></i></button></a></li>
-          <?php }?>
-            <?php if ($seskdlvl=='Umum' || $seskdlvl=='Admin'){?>
-          <li style="display: block;margin:0;padding:0;float: left;"><a href="?p=part" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Part <i class="fa fa-wrench"></i></button></a></li>
-          <?php }?>
-         <?php if ($seskdlvl=='Admin'){?>
-           <li class="dropdown" style="display: block;margin:0;padding:0;float: left;">
-            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Finance BR <i class="fa fa-usd dropdown-toggle"></i></button> </a>
-            <ul class="dropdown-menu">
-              <li class="header"><a href="?p=kwitansi">Kwitansi BR</a></li> 
-              <li class="header"><a href="?p=kwitansior">Kwitansi OR BR</a></li>   
-               <li class="header"><a href="?p=cash">Cash BR</a></li> 
-               <li class="header"><a href="?p=bank">Bank BR</a></li> 
-              <li class="header"><a href="?p=gatepass">Gate Pass BR</a></li> 
-                          </ul>
-          </li><?php } ?>
-          <?php if ($seskdlvl=='Admin'){?>
-           <li class="dropdown" style="display: block;margin:0;padding:0;float: left;">
-            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Finance GR <i class="fa fa-usd dropdown-toggle"></i></button> </a>
-            <ul class="dropdown-menu">
-              <li class="header"><a href="?p=kwitansigr">Kwitansi GR</a></li> 
-               <li class="header"><a href="?p=cashgr">Cash GR</a></li> 
-               <li class="header"><a href="?p=bankgr">Bank GR</a></li> 
-               <li class="header"><a href="?p=gatepassgr">Gate Pass GR</a></li> 
-               <li class="header"><a href="?p=paycuci">Payment Non PKB</a></li> 
-                <!--<li class="header"><a href="?p=paypkbjasa">Payment PKB Jasa</a></li>-->
-          </ul>
-          </li><?php } ?>
+          
             <?php if ($seskdlvl=='Admin'){?>
            <li class="dropdown" style="display: block;margin:0;padding:0;float: left;">
-            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Accounting <i class="fa fa-usd dropdown-toggle"></i></button> </a>
+            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">ACCOUNTING <i class="fa fa-usd dropdown-toggle"></i></button> </a>
             <ul class="dropdown-menu">
-              <li class="header"><a href="?p=mcoa">Master COA</a></li> 
-              <li class="header"><a href="?p=acccash">Cash</a></li>  
-              <li class="header"><a href="?p=accbank">Bank</a></li>   
+              <li class="header"><a href="?p=mcoa">MASTER COA</a></li> 
+              <li class="header"><a href="?p=acccash">CASH</a></li>  
+              <li class="header"><a href="?p=accbank">BANK</a></li>   
                
                           </ul>
           </li><?php } ?>
-          <li class="dropdown" style="display: block;margin:0;padding:0;float: left;">
-            <a href="?p=laporan" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Laporan <i class="fa fa-gears dropdown-toggle"></i></button> </a>
-            <!--<ul class="dropdown-menu">
-              <?php if ($seskdlvl=='SuratMasuk' || $seskdlvl=='Admin'){?>
-              <li class="header"><a href="?p=lapsuratmasuk">Lap. PKB</a></li><?php }?>
-              <?php if ($seskdlvl=='SuratKeluar' || $seskdlvl=='Admin'){?>
-              <li class="header"><a href="?p=lapsuratkeluar">Lap. PKB Batal</a></li><?php }?>
-              
-            </ul>-->
-          </li>
+         
           <?php if ($seskdlvl=='Admin'){?>
            <li class="dropdown" style="display: block;margin:0;padding:0;float: left;">
-            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Master <i class="fa fa-gears dropdown-toggle"></i></button> </a>
+            <a href="#" data-toggle="dropdown" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">MASTER <i class="fa fa-gears dropdown-toggle"></i></button> </a>
             <ul class="dropdown-menu">
-              <li class="header"><a href="?p=customer">Customer</a></li> 
-              <li class="header"><a href="?p=asuransi">Asuransi</a></li>   
-              <li class="header"><a href="?p=supplier">Supplier</a></li>
-              <li class="header"><a href="?p=partnerbank">Partner Bank</a></li>
-              <li class="header"><a href="?p=satuan">Satuan</a></li>
-              <li class="header"><a href="?p=part">Part</a></li>
-              <li class="header"><a href="?p=panel">Panel</a></li>   
-              <li class="header"><a href="?p=group">Group Kendaraan</a></li>   
-              <li class="header"><a href="?p=tipe">Tipe Kendaraan</a></li>   
-              <li class="header"><a href="?p=warna">Warna Kendaraan</a></li>   
-              <li class="header"><a href="?p=cuci">Cuci Mobil</a></li>   
-              <li class="header"><a href="?p=salon">Salon Mobil</a></li> 
-              <li class="header"><a href="?p=jasa">Jasa</a></li> 
-              <li class="header"><a href="?p=paketjasa">Paket Jasa</a></li> 
+              <li class="header"><a href="?p=customer">CUSTOMER</a></li> 
+              <li class="header"><a href="?p=asuransi">ASURANSI</a></li>   
+              <li class="header"><a href="?p=supplier">SUPPLIER</a></li>
+              <li class="header"><a href="?p=partnerbank">PARTNER BANK</a></li>
+              <li class="header"><a href="?p=satuan">SATUAN</a></li>
+              <li class="header"><a href="?p=part">PART</a></li>
+              <li class="header"><a href="?p=panel">PANEL</a></li>   
+              <li class="header"><a href="?p=group">GROUP KENDARAAN</a></li>   
+              <li class="header"><a href="?p=tipe">TIPE KENDARAAN</a></li>   
+              <li class="header"><a href="?p=warna">WARNA KENDARAAN</a></li>   
+              <li class="header"><a href="?p=cuci">CUCI MOBIL</a></li>   
+              <li class="header"><a href="?p=salon">SALON MOBIL</a></li> 
+              <li class="header"><a href="?p=jasa">JASA</a></li> 
+              <li class="header"><a href="?p=paketjasa">PAKET JASA</a></li> 
               <li class="header">----------------------------------</li>
-              <li class="header"><a href="?p=backup">Backup Database</a></li>
-            <!--   <li class="header"><a href="?p=user">User</a></li>   
-              <li class="header"><a href="?p=company">Company</a></li> 
-              <li class="header"><a href="?p=backup">Backup Database</a></li>
-              <li class="header"><a href="?p=restore">Restore Database</a></li>
-              <li class="header"><a href="?p=backupfile">Backup File</a></li> -->
+              <li class="header"><a href="?p=backup">BACKUP DATABASE</a></li>            
             </ul>
           </li><?php } ?>
 
           <li class="dropdown" style="display: block;margin:0;padding:0;float: left;" class="dropdown notifications-menu">
-            <a href="login/logout.php" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">Logout <i class="fa fa-sign-out"></i></button></a></li>
+            <a href="login/logout.php" style="width: 100%;padding: 0.5em;"><button class="btn btn-default" style="padding: 5px 10px;">LOGOUT <i class="fa fa-sign-out"></i></button></a></li>
 
           </li>
 
@@ -125,7 +103,9 @@
       </div>
     </div>
     </nav>
-  </header>
+
+ 
+    </header>
 
  <style>
  table.dataTable tbody th, table.dataTable tbody td {
@@ -231,5 +211,52 @@ table.dataTable thead tr, table.dataTable thead tr {
     background-color:white;
     border-width: 1px;
   }
-  
+  /* HEADER MENU*/
+  .dropdown-submenu {
+    position: relative;
+}
+
+.dropdown-submenu>.dropdown-menu {
+    top: 0;
+    left: 100%;
+    margin-top: -6px;
+    margin-left: -1px;
+    -webkit-border-radius: 0 6px 6px 6px;
+    -moz-border-radius: 0 6px 6px;
+    border-radius: 0 6px 6px 6px;
+}
+
+.dropdown-submenu:hover>.dropdown-menu {
+    display: block;
+}
+
+.dropdown-submenu>a:after {
+    display: block;
+    content: " ";
+    float: right;
+    width: 0;
+    height: 0;
+    border-color: transparent;
+    border-style: solid;
+    border-width: 5px 0 5px 5px;
+    border-left-color: #ccc;
+    margin-top: 5px;
+    margin-right: -10px;
+}
+
+.dropdown-submenu:hover>a:after {
+    border-left-color: #fff;
+}
+
+.dropdown-submenu.pull-left {
+    float: none;
+}
+
+.dropdown-submenu.pull-left>.dropdown-menu {
+    left: -100%;
+    margin-left: 10px;
+    -webkit-border-radius: 6px 0 6px 6px;
+    -moz-border-radius: 6px 0 6px 6px;
+    border-radius: 6px 0 6px 6px;
+}
  </style>
