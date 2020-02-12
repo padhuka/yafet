c4c13e17e56e31e5ae12cf7dcb461b94b773161e
padhuka <dhuka@ssonlab.com>
2020-02-12 20:44:18 +0700

    update title

--- pages/footer.php
+++ pages/footer.php
@@ -3,7 +3,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2019 <a href="#"><?php echo $titlefooter;?></a></strong>
+    <strong>Copyright &copy; 2020 <a href="#"><?php echo $titlefooter;?></a></strong>
   </footer>
 
   <!-- Control Sidebar -->
--- pages/header_menu.php
+++ pages/header_menu.php
@@ -6,7 +6,7 @@
     <nav class="navbar fixed-top" style="margin-left: 0px" >
       <!-- Sidebar toggle button-->
     <div class="container">
-      <a class="navbar-brand" href="index.php">Alfa Omega
+      <a class="navbar-brand" href="index.php">.: ALFA OMEGA :.
         </a>
       <div class="navbar-custom-menu" style="padding: 0 0 0 0;margin: 0 auto;">
         <ul class="nav navbar-nav" style="list-style: none;padding: 0;margin: 0 auto;font-size: 1px;margin-top: 8px;">
